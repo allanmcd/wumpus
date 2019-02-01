036dbd1140d57c508692d499839ec7f054535f4a
Allan McDaniel <allan@mcdaniel.net>
2019-02-01 12:05:17 -0800

    added Map.java to repository

---
+++ src/com/jetbrains/Map.java
@@ -0,0 +1,9 @@
+package com.jetbrains;
+
+//
+// NOTE there should only be one Map object
+//
+public class Map {
+    // used to keep track of where the bats are currently located
+    int batRooms[] = new int[2];
+}
