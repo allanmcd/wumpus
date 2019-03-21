a6109219d070466ffecaf0a721f88650ae31bf4f
Allan McDaniel <allan@mcdaniel.net>
2019-03-20 20:54:40 -0700

    comment changes

--- src/com/jetbrains/Debug.java
+++ src/com/jetbrains/Debug.java
@@ -7,20 +7,15 @@ import javafx.scene.control.ButtonType;
 // NOTE there should only be one Debug object
 //
 class Debug {
-    //
-    // Debug static variables
-    //
+    ////////////////////////////
+    // Debug static variables //
+    ///////////////////////////
+
     static boolean isEnabled = false;
 
-    static void log(String msg){
-        if(isEnabled){
-            System.out.println(msg);
-        }
-    }
-
-    //
-    // Debug methods
-    //
+    ///////////////////
+    // Debug methods //
+    ///////////////////
 
     static void error(String alertMsg){
         javafx.scene.control.Alert alert = new javafx.scene.control.Alert(Alert.AlertType.ERROR,alertMsg, ButtonType.OK);
@@ -32,4 +27,9 @@ class Debug {
         alert.showAndWait();
     }
 
+    static void log(String msg){
+        if(isEnabled){
+            System.out.println(msg);
+        }
+    }
 }
